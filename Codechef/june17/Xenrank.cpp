3b24f2e972c9bbfb206f1574be204f64c5d764d4
Aditya Anantharaman <adityanant@gmail.com>
2017-06-23 11:16:40 +0530

    Create Xenrank.cpp

---
+++ Codechef/june17/Xenrank.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	long long t;
+	cin>>t;
+	
+ 
+	
+	while(t--)
+	{
+	    long long u,v;
+	    cin>>u>>v;
+	    
+	    
+	    cout<<(u+v)*(u+v+1)/2+(u+1)<<endl;
+	    
+	}
+	
+	
+	return 0;
+}
+ 
